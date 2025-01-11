b04e90f6bf7326f11a9ff468b5f4c89b85cdf5fb
fakegura <jikangtune@proton.me>
2025-01-11 14:21:48 +0700

    Update azure_ubuntu_w-okada_installation.sh

--- azure_ubuntu_w-okada_installation.sh
+++ azure_ubuntu_w-okada_installation.sh
@@ -10,9 +10,11 @@ yes | sudo apt-get install --only-upgrade libstdc++6
 cd /tmp
 curl -O https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
 bash Anaconda3-2024.10-1-Linux-x86_64.sh -b
+
 cd /home/azureuser/
 anaconda3/bin/conda init
 source ~/.bashrc
+
 yes | conda create -n vcclient-dev python=3.10
 conda activate vcclient-dev
 
@@ -21,9 +23,12 @@ cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 /home/azureuser/anaconda3/envs/vccli
 pip install pip==24.0
 pip install fairseq
 pip install pyworld
+
 git clone https://github.com/w-okada/voice-changer.git
 cd /home/azureuser/voice-changer/server
 pip install -r requirements.txt
+
+server_ip=$(curl -s http://ifconfig.me)
 python3 MMVCServerSIO.py -p 18888 --https true \
     --content_vec_500 pretrain/checkpoint_best_legacy_500.pt  \
     --content_vec_500_onnx pretrain/content_vec_500.onnx \
@@ -38,4 +43,4 @@ python3 MMVCServerSIO.py -p 18888 --https true \
     --model_dir model_dir \
     --samples samples.json \
     --host 0.0.0.0 \
-    --allowed-origins https://172.188.41.3:18888 
+    --allowed-origins https://$server_ip:18888 
